6cdbd5c4cbe7b9db56df4da97681e30b7fd3c7fb
ParkMiram <myurm@naver.com>
2024-06-12 09:06:01 +0900

    todo

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -12,26 +12,52 @@ const Todo = () => {
                 <div className="modalWrap">
                     <div className="modalHeader">
                         <p className="category">생활</p>
-                        <button className="menu"></button>
+                        <div>
+                            <button className="menu"></button>
+                            <div>
+                                <button>수정하기</button>
+                                <button>삭제하기</button>
+                            </div>
+                        </div>
                     </div>
                     <div className="modalBody">
                         <div className="todo">
                             <Checkbox id={`1`} />
-                            <p className="title">방 청소하기</p>
+                            <p className="todoTitle">방 청소하기</p>
                             <p className="date">06/11</p>
                         </div>
                         <p className="todoDesc">컴퓨터방, 침대방 꼼꼼히 청소하기</p>
                     </div>
                     <ul className="todoComment">
                         <li className="comment">
-                            <img src={basicProfile} alt={"프로필 사진"} />
-                            <div>
+                            <img src={basicProfile} alt={"프로필 사진"}/>
+                            <div className="commentInfo">
                                 {/* TODO 댓글부터 */}
-                                <p></p>
-                                <p></p>
+                                <div className="commentHead">
+                                    <p className="nickname">bootjar</p>
+                                    <p className="date">2024-06-11</p>
+                                </div>
+                                <p className="text">아 몰라앙!</p>
                             </div>
+                            <button className="menu"></button>
+                        </li>
+                        <li className="comment">
+                            <img src={basicProfile} alt={"프로필 사진"}/>
+                            <div className="commentInfo">
+                                {/* TODO 댓글부터 */}
+                                <div className="commentHead">
+                                    <p className="nickname">sort</p>
+                                    <p className="date">2024-06-11</p>
+                                </div>
+                                <p className="text">미람아,</p>
+                            </div>
+                            <button className="menu"></button>
                         </li>
                     </ul>
+                    <div className="postComment">
+                        <input type="text" placeholder="댓글 입력" />
+                        <button className="send"></button>
+                    </div>
                 </div>
             </div>
         </>
