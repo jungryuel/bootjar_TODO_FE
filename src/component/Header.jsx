0ee65c757f8af344388f5a812c0eb11776ffcfff
jungryuel <zldrktduf0927@naver.com>
2024-06-28 20:24:08 +0900

    로그인 로그아웃

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import "../styles/header.css";
-// img
 import logo from "../assets/images/logo.svg";
 import friendRequest from "../assets/images/friendRequest.svg";
 import RequestFriendList from "./RequestFriendList.jsx";
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+    const navigate = useNavigate();
+
+    const logout = () => {
+        localStorage.removeItem('token');
+        navigate('/login');
+    };
+
     return (
         <>
             <div className="headerContainer">
@@ -17,11 +24,12 @@ const Header = () => {
                     </div>
                 </div>
                 <div className="headerOption">
-                    <button className="friendRequest"><img src={friendRequest} alt={"친구요청"}/>
+                    <button className="friendRequest">
+                        <img src={friendRequest} alt={"친구요청"}/>
                         <RequestFriendList></RequestFriendList>
                     </button>
                     <input type="text" className="search" placeholder="친구 검색"/>
-                    <button className="logout">로그아웃</button>
+                    <button className="logout" onClick={logout}>로그아웃</button>
                 </div>
             </div>
         </>
